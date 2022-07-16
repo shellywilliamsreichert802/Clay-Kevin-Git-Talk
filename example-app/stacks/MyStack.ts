ba1bac8d0c0f3df1adae58d571c6cffadfe86066
Clay Boley <ctboley@proton.me>
2022-07-16 11:51:51 -0500

    Add example app (#1)
    * init commit
    
    * Rename folder
    
    * Create CODEOWNERS
    
    Co-authored-by: Clay Boley <ctboley@protonmail.com>

---
+++ example-app/stacks/MyStack.ts
@@ -0,0 +1,26 @@
+import { Api, StackContext, ViteStaticSite } from "@serverless-stack/resources";
+
+export function MyStack({ stack }: StackContext) {
+  // Create the HTTP API
+  const api = new Api(stack, "Api", {
+    routes: {
+      "GET /notes": "functions/list.handler",
+      "GET /notes/{id}": "functions/get.handler",
+      "PUT /notes/{id}": "functions/update.handler",
+    },
+  });
+
+  // Deploy our React app
+  const site = new ViteStaticSite(stack, "Site", {
+    path: "web",
+    environment: {
+      VITE_API_URL: api.url,
+    },
+  });
+
+  // Show the URLs in the output
+  stack.addOutputs({
+    SiteUrl: site.url,
+    ApiEndpoint: api.url,
+  });
+}
